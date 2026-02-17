70b8081cc98b45f78e09e36e71b7c20197b64cdc
julienexe <julianssonko@gmail.com>
2026-02-17 12:46:01 +0300

    add production details

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+
+const app = express();
+const buildPath = path.join(__dirname, 'build');
+
+app.use(express.static(buildPath));
+
+app.use((req, res) => {
+  res.sendFile(path.join(buildPath, 'index.html'));
+});
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
